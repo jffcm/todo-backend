7e6ef9cab3f1f8bca9762a6d96904915e5b01188
João Francisco Franco Cançado Martins <joao.jffc@gmail.com>
2024-08-08 15:29:46 -0300

    feat: configura servidor Express com Swagger para documentação da API e rotas

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const usuarioRoutes = require('./routes/usuarioRoutes');
+const tarefaRoutes = require('./routes/tarefaRoutes')
+const db = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use('/api/v1', usuarioRoutes);
+app.use('/api/v1', tarefaRoutes);
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    explorer: true,
+    info: {
+        title: "Todo",
+        version: "1.0.0",
+        description: "API para gerenciamento de tarefas"
+    },
+    components: {
+        schemas: require('../swagger.json'),
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT'
+            }
+        }
+    }
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['src/routes/*.js']
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+const PORT = process.env.PORT || 3000;
+
+db.connectDB();
+
+app.listen(PORT, () => {
+    console.log(`O servidor está rodando na porta ${PORT}`);
+});
