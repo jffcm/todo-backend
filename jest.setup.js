5970d57d4423cac55ab46dd8905e3f5187b05122
João Francisco Franco Cançado Martins <joao.jffc@gmail.com>
2024-08-09 14:20:00 -0300

    chore: configura MongoMemoryServer para testes unitários em memória

---
+++ jest.setup.js
@@ -0,0 +1,23 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
+
+afterEach(async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany({});
+    }
+});
